f3c80a01e956754691e774d138f75ff23243d8d3
luna <85889752+Miyakomoon@users.noreply.github.com>
2023-03-27 16:27:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-# The URL of the website containing the embedded Excel sheets
+# The URL of the website containing the embedded Google sheets
 url = 'https://equipezgt.blogspot.com/p/liga-zgt-tabela-de-classificacao_16.html'
 
 # Setting up the Selenium driver
 print('Acessando o site...')
 options = Options()
-options.add_argument('--headless')  # Run Chrome in headless mode (no GUI)
+options.add_argument('--headless')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.get(url)
 index = 1
 dfs = []
 
-# Finding the links of the spreadsheets
+# Finding the links of the documents
 links = driver.find_elements("xpath", "//iframe[contains(@src, 'spreadsheets')]")
 
-# Spliting each Excel sheet
+# Spliting each Goggle sheet
 for link in links:
     result = str(link.get_attribute("src"))
     sheet_url = result
@@ -30,7 +30,6 @@ for link in links:
         df_list = pd.read_html(sheet_url, header=0, encoding='utf-8')
 
         if len(df_list) > 0:
-            # Select the first data frame
             df = df_list[0]
 
             # Append the DataFrame to the list of DataFrames
@@ -45,15 +44,11 @@ for link in links:
 # Concatenate all the DataFrames into a single DataFrame
 final_df = pd.concat(dfs, ignore_index=True)
 
-# Create a new workbook
+# Saving the data into an Excel file
 workbook = pd.ExcelWriter('zgt_tabelas.xlsx')
-
-# Write the data to the new workbook
 final_df.to_excel(workbook, index=False)
-
-# Save the workbook
 workbook.save()
 print("Todas as tabelas foram salvas corretamente!")
 
-# Clean up the Selenium driver
+# Cleans up the Selenium driver
 driver.quit()
